4ece392c08d91bad206073f6578dd4f5f16f8781
paulocesarcit <paulocesarcit@hotmail.com>
2019-04-05 19:31:25 -0300

    init

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
