e64081c3b1e5066567114c57d307b226b8010936
paulocesarcit <paulocesarcit@hotmail.com>
2019-04-05 22:20:31 -0300

    recaptcha with error on submit

--- server.js
+++ server.js
@@ -6,3 +6,40 @@ const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+
+app.get('/', (req, res, next) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/subscribe', (req, res, next) => {
+    if (
+        req.body.captcha === undefined ||
+        req.body.captcha === '' ||
+        req.body.captcha === null
+    ){
+        return res.json({"success": false, "msg": "Please select Captcha"})
+    }
+
+    // Secrect Key
+    const secretKey = '6LfniZwUAAAAACocVPfurssN_5cWgi-ppM6SEfYP';
+
+    // Verify URL
+    const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}&remoteip=${req.connection.remoteAddress}`
+
+    // Make Request to VerifyURL
+    request(verifyUrl, (err, res, body) => {
+        body = JSON.parse(body)
+
+        // If not Successful
+        if (body.success !== undefined && !body.success){
+            return res.json({"success": false, "msg": "Failed Captcha Verification"})
+        }
+
+        // If Successful
+        return res.json({"success": true, "msg": "Captcha Passed"})
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server Started on port 3000')
+})
